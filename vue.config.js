722e69bfee81881fcd37c70e1dd569f90ac1b694
marua <meb@mettrr.com>
2019-11-26 20:16:43 +0000

    deployment stuff

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/email-composer/'
+    ? '/vue-email-composer/'
     : '/',
   transpileDependencies: ['vuetify'],
   pluginOptions: {
