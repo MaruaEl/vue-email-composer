0092d2780bd2a222917aeb74be32c09efd188d9a
marua <meb@mettrr.com>
2019-11-26 22:09:04 +0000

    first little test

--- README.md
+++ README.md
@@ -1,34 +1,31 @@
-# vue-email-composer
+# Vue email Composer
+![](./public/img/readme-img.png)
+This is Vue email composer is built using Vuetify, a Material Design based Framework, a reusable semantic component framework for Vue.js
+The project was initialized using Vue Cli-3.
+
+The form data is handled using a simple Vuex store with no modules.
+
+I've Used global reusable components for good practice, actions are dispatched from the parent view.
+vue.config.js loada the `main.scss` file and makes the global styles available globally - to avoid an import in every component.
+
+Testing made using the [vue-test utility](https://vue-test-utils.vuejs.org/)
+
+-----
 
 ## Project setup
 ```
 yarn install
 ```
-
 ### Compiles and hot-reloads for development
 ```
-yarn run serve
+yarn run dev
+```
+### Run tests
+
+```
+yarn test:unit
 ```
 
-### Compiles and minifies for production
-```
-yarn run build
-```
+See Vuetify docs [Docs Reference](https://vuetifyjs.com/en/getting-started/).
 
-### Run your tests
-```
-yarn run test
-```
-
-### Lints and fixes files
-```
-yarn run lint
-```
-
-### Run your unit tests
-```
-yarn run test:unit
-```
-
-### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- tests/unit/form.spec.js
+++ tests/unit/form.spec.js
@@ -2,12 +2,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 
+
 // Utilities
-import { mount, createLocalVue } from '@vue/test-utils';
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
 
 // Components
 import ButtonComponent from '@/components/global/ButtonComponent';
 
+Vue.use(Vuetify);
 const localVue = createLocalVue();
 
 describe('ButtonComponent.vue', () => {
@@ -18,44 +20,14 @@ describe('ButtonComponent.vue', () => {
   });
 
   it('It should have a button text', () => {
-    const wrapper = mount(ButtonComponent, {
+    const text = 'Send';
+    const wrapper = shallowMount(ButtonComponent, {
       localVue,
       vuetify,
       propsData: {
-        buttonText: 'Send',
+        buttonText: text,
       },
     });
-
-    // With jest we can create snapshot files of the HTML output
-    expect(wrapper.html()).toMatchSnapshot();
-
-    // We could also verify this differently
-    // by checking the text content
-    const title = wrapper.find('button');
-
-    expect(title.text()).toBe('send');
-  });
-
-  it('should emit an event when the action v-btn is clicked', () => {
-    const wrapper = mount(ButtonComponent, {
-      localVue,
-      vuetify,
-    });
-
-    const event = jest.fn();
-    const button = wrapper.find('.v-btn');
-
-    // Here we bind a listener to the wrapper
-    // instance to catch our custom event
-    // https://vuejs.org/v2/api/#Instance-Methods-Events
-    wrapper.vm.$on('action-btn:clicked', event);
-
-    expect(event).toHaveBeenCalledTimes(0);
-
-    // Simulate a click on the button
-    button.trigger('click');
-
-    // Ensure that our mock event was called
-    expect(event).toHaveBeenCalledTimes(1);
+    expect(wrapper.text()).toMatch(text);
   });
 });
