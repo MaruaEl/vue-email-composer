cdbb10b434deb878da1302e5dcef59750186a4ea
marua <meb@mettrr.com>
2019-11-28 08:28:03 +0000

    Updates

---
+++ precache-manifest.92b0a37855d6fee14f085f149ccfb5e9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096de87647ccf0fb305b",
+    "url": "/vue-email-composer/css/app.b40cede4.css"
+  },
+  {
+    "revision": "b038ebbca7539402daa8",
+    "url": "/vue-email-composer/css/chunk-vendors.34386301.css"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/vue-email-composer/fonts/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/vue-email-composer/fonts/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/vue-email-composer/fonts/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/vue-email-composer/fonts/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/vue-email-composer/fonts/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/vue-email-composer/fonts/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/vue-email-composer/fonts/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/vue-email-composer/fonts/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/vue-email-composer/fonts/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/vue-email-composer/fonts/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/vue-email-composer/fonts/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/vue-email-composer/fonts/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/vue-email-composer/img/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/vue-email-composer/img/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/vue-email-composer/img/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "440ab59dcd063a6f19abf85f666b1f4c",
+    "url": "/vue-email-composer/img/readme-img.png"
+  },
+  {
+    "revision": "fac6023abb0a8d28a9cf09544d904bbe",
+    "url": "/vue-email-composer/index.html"
+  },
+  {
+    "revision": "096de87647ccf0fb305b",
+    "url": "/vue-email-composer/js/app.22aa0692.js"
+  },
+  {
+    "revision": "b038ebbca7539402daa8",
+    "url": "/vue-email-composer/js/chunk-vendors.853d1199.js"
+  },
+  {
+    "revision": "b79a1c043083f410f22376ea3ab66e07",
+    "url": "/vue-email-composer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-email-composer/robots.txt"
+  }
+]);
